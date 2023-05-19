1913f388ff20b1f41b64ef7563f29c2bbbaca5de
hamidie00235 <ahmathisseinh@gmail.com>
2023-05-20 01:14:20 +0300

    layout component is created

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
    <div>
-    <Navbar/>
+
     <h1>Homepage</h1>
     <p>baskı testi yapacakken örnek 
             metinlere ihtiyaç duyduğumuzda kullanabileceğimiz 
@@ -20,9 +20,9 @@ export default function Home() {
             metinlere ihtiyaç duyduğumuzda kullanabileceğimiz 
             metinlerdir</p>
             <Link href="/ninjas" >
-            <a>See Ninja List</a>
+            See Ninja List
             </Link>
-            <Footer/>
+           
    </div>
-  )
+  );
 }
